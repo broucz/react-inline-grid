d9355595b7a2437d82e77e541823eef5c746f0d1
broucz <broucapierre@gmail.com>
2015-08-20 14:28:54 +0200

    add minimal "dev" environment as example

---
+++ examples/dev/components/Root.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Grid, Row, Cell } from 'react-inline-grid';
+
+const b = {
+  gray: { background: '#cccccc' },
+  primary: { background: '#4285F4' }
+};
+const options = [
+  {
+    name: 'desktop',
+    gutter: 16,
+    margin: 16,
+    columns: 12,
+    order: 2,
+    query: '(min-width: 840px)'
+  },
+  {
+    name: 'tablet',
+    gutter: 16,
+    margin: 16,
+    columns: 8,
+    order: 1,
+    query: '(min-width: 480px) and (max-width: 839px)'
+  },
+  {
+    name: 'phone',
+    gutter: 16,
+    margin: 16,
+    columns: 4,
+    order: 0,
+    query: '(max-width: 479px)'
+  }
+];
+
+class Root extends Component {
+  render() {
+    return (
+      <Grid options={options}>
+        <Row is="center">
+          <Cell is="11 tablet-4 phone-4">
+            <div className="box" style={b.gray}></div>
+          </Cell>
+        </Row>
+      </Grid>
+    );
+  }
+}
+
+export default Root;
