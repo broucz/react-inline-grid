e54df66396695ecf58d29996a0cf954e3d790c61
broucz <broucapierre@gmail.com>
2015-08-21 14:22:54 +0200

    update gh-page

--- examples/gh-pages/index.js
+++ examples/gh-pages/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/Root';
 
-ReactDOM.render(<Root />, document.querySelector('#mount'));
+import 'styles/main';
+
+ReactDOM.render(<Root />, document.getElementById('mount'));
