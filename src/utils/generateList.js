d50df348f2fd381a2cff7bd81459b326f2a6cc88
broucz <broucapierre@gmail.com>
2015-08-25 02:01:59 +0200

    remove `fromJS` in generateList

--- src/utils/generateList.js
+++ src/utils/generateList.js
@@ -1,14 +1,21 @@
-import { fromJS, List } from 'immutable';
+import { List } from 'immutable';
 
 /**
- * tag = 'col'
+ * Return an Immutable List with component `tag` as first
+ * entry following buy `input` values separated by whitespace.
+ *
+ * tag = 'cell'
  * input = '6 phone-2'
  *
- * output = ['col', '6', 'phone-2']
+ * return List['cell', '6', 'phone-2']
+ *
+ * @param {String} tag
+ * @param {String} input
+ * @returns {List}
  */
-export default function generateList(tag, schema) {
-  if (schema && schema.trim()) {
-    const clean = fromJS(schema.trim().split(' '));
+export default function generateList(tag, input) {
+  if (input && input.trim()) {
+    const clean = List.of(...input.trim().split(/\s+/));
     return clean.unshift(tag);
   }
   return new List([tag]);
