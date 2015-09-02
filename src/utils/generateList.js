cbd46215c3ad0185453b9bcefe34bc3e1d5598dc
broucz <broucapierre@gmail.com>
2015-09-02 11:19:58 +0200

    other

--- src/utils/generateList.js
+++ src/utils/generateList.js
@@ -1,15 +1,15 @@
 /**
- * Return an Immutable List with component `tag` as first
+ * Return an array with component `tag` as first
  * entry following buy `input` values separated by whitespace.
  *
  * tag = 'cell'
  * input = '6 phone-2'
  *
- * return List['cell', '6', 'phone-2']
+ * return ['cell', '6', 'phone-2']
  *
  * @param {String} tag
  * @param {String} input
- * @returns {List}
+ * @returns {Array}
  */
 export default function generateList(tag, input) {
   if (input && input.trim()) {
